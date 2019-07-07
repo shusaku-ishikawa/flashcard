daebfbab3792f4a80ed3f6faea335cf528b49327
root <root@os3-369-17689.vs.sakura.ne.jp>
2019-07-07 13:52:15 +0900

    a

--- flashcard/flashcard/settings.py
+++ flashcard/flashcard/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = '/static/'
+STATIC_URL = '/static_flashcard/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'flashcardapp/static'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media_flashcard/'
